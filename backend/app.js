27de7f37c0fe4b13dd9b6ea5a8c41dffa58df1d0
Austin Jacobs <aust.t.jac@gmail.com>
2022-12-11 14:22:32 -0700

    Removed Mongo URI from app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,13 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const Post = require("./models/post");
+const Post = require('./models/post');
 
 const app = express();
 
 mongoose
-  .connect(
-    'mongodb+srv://austin:Atj12fb12@cluster0.4k7guo9.mongodb.net/node-angular?retryWrites=true&w=majority'
-  )
+  .connect(process.env.DB_CONNECTION_STRING)
   .then(() => {
     console.log('Connected to database!');
   })
@@ -21,42 +19,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
   );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
   const post = new Post({
     title: req.body.title,
-    content: req.body.content
+    content: req.body.content,
   });
   post.save();
   res.status(201).json({
-    message: "Post added successfully"
+    message: 'Post added successfully',
   });
 });
 
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then(documents => {
+app.get('/api/posts', (req, res, next) => {
+  Post.find().then((documents) => {
     res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents
+      message: 'Posts fetched successfully!',
+      posts: documents,
     });
   });
 });
 
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
     console.log(result);
-    res.status(200).json({ message: "Post deleted!" });
+    res.status(200).json({ message: 'Post deleted!' });
   });
 });
 
